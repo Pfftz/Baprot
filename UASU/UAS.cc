eee99735b54d07ecfe47d02b7cff05f2d34652c6
Pfftz <Kalixpfft+github@protonmail.com>
2023-12-12 12:06:43 +0700

    Add functions and operations
    This commit adds various functions and operations to the code, including void functions, functions with arguments, functions with return values, recursive functions, prototypes, string operations, and array operations.

---
+++ UASU/UAS.cc
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void fungsiVoid()
+{
+    cout << "Ini adalah fungsi void" << endl;
+}
+
+void fungsitanpaArgumen()
+{
+    cout << "Ini adalah fungsi tanpa argumen" << endl;
+}
+
+void fungsidenganArgumen(int a)
+{
+    cout << "Ini adalah fungsi dengan argumen" << endl;
+    cout << "Nilai yang anda masukkan adalah " << a << endl;
+}
+
+int fungsidenganNilaiKembali()
+{
+    cout << "Ini adalah fungsi dengan nilai kembali" << endl;
+    return 10;
+}
+
+int fungsidenganNilaiKembaliDanArgumen(int a)
+{
+    cout << "Ini adalah fungsi dengan nilai kembali dan argumen" << endl;
+    cout << "Nilai yang anda masukkan adalah " << a << endl;
+    return 10;
+}
+
+int fungsiRekursif(int a)
+{
+    cout << "Ini adalah fungsi rekursif" << endl;
+    cout << "Nilai yang anda masukkan adalah " << a << endl;
+    if (a == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return fungsiRekursif(a - 1);
+    }
+}
+
+int prototype(int a, int b);
+
+void operasiString()
+{
+    cout << "Ini adalah operasi string" << endl;
+    string nama = "UASU";
+    cout << "Nama saya adalah " << nama << endl;
+    cout << "Panjang string adalah " << nama.length() << endl;
+    cout << "String ke 2 adalah " << nama[1] << endl;
+    cout << "Mengganti string ke 2 dengan 'A' " << endl;
+    nama[1] = 'A';
+    cout << "Nama saya adalah " << nama << endl;
+    cout << "Mencari posisi string 'A' " << endl;
+    cout << "Posisi string 'A' adalah " << nama.find('A') << endl;
+    cout << "Mengambil substring dari posisi 1 sebanyak 2 karakter " << endl;
+    cout << "Substring adalah " << nama.substr(1, 2) << endl;
+}
+
+void array1Dimensi()
+{
+    cout << "Ini adalah array 1 dimensi" << endl;
+    int array1[5] = {1, 2, 3, 4, 5};
+    cout << "Array ke 2 adalah " << array1[1] << endl;
+    cout << "Mengganti array ke 2 dengan 10 " << endl;
+    array1[1] = 10;
+    cout << "Array ke 2 adalah " << array1[1] << endl;
+    cout << "Mengambil panjang array " << endl;
+    cout << "Panjang array adalah " << sizeof(array1) / sizeof(array1[0]) << endl;
+}
+
+void array2Dimensi()
+{
+    cout << "Ini adalah array 2 dimensi" << endl;
+    int array2[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    cout << "Array ke 2 adalah " << array2[1][1] << endl;
+    cout << "Mengganti array ke 2 dengan 10 " << endl;
+    array2[1][1] = 10;
+    cout << "Array ke 2 adalah " << array2[1][1] << endl;
+    cout << "Mengambil panjang array " << endl;
+    cout << "Panjang array adalah " << sizeof(array2) / sizeof(array2[0]) << endl;
+}
+
+void array3Dimensi()
+{
+    cout << "Ini adalah array 3 dimensi" << endl;
+    int array3[2][3][2] = {{{1, 2}, {3, 4}, {5, 6}}, {{7, 8}, {9, 10}, {11, 12}}};
+    cout << "Array ke 2 adalah " << array3[1][1][1] << endl;
+    cout << "Mengganti array ke 2 dengan 10 " << endl;
+    array3[1][1][1] = 10;
+    cout << "Array ke 2 adalah " << array3[1][1][1] << endl;
+    cout << "Mengambil panjang array " << endl;
+    cout << "Panjang array adalah " << sizeof(array3) / sizeof(array3[0]) << endl;
+}
+
+int main()
+{
+    int pilihan;
+    bool isRunning = true;
+    while (isRunning)
+    {
+        cout << "+----------------------------------------- +" << endl;
+        cout << " Program Sederhana C++" << endl;
+        cout << " Menu Utama" << endl;
+        cout << " 1. Fungsi Void" << endl;
+        cout << " 2. Fungsi tanpa argumen" << endl;
+        cout << " 3. Fungsi dengan argumen" << endl;
+        cout << " 4. Fungsi dengan nilai kembali" << endl;
+        cout << " 5. Fungsi dengan nilai kembali dan argumen" << endl;
+        cout << " 6. Fungsi rekursif" << endl;
+        cout << " 7. Prototype" << endl;
+        cout << " 8. Operasi String" << endl;
+        cout << " 9. Array 1 Dimensi" << endl;
+        cout << " 10. Array 2 Dimensi" << endl;
+        cout << " 11. Array 3 Dimensi" << endl;
+        cout << " 0. Keluar" << endl;
+        cout << "+----------------------------------------- +" << endl;
+        cout << " Masukkan pilihan anda : ";
+        cin >> pilihan;
+        switch (pilihan)
+        {
+        case 1:
+            cout << "Anda memilih fungsi void" << endl;
+            break;
+        case 2:
+            cout << "Anda memilih fungsi tanpa argumen" << endl;
+            break;
+        case 3:
+            cout << "Anda memilih fungsi dengan argumen" << endl;
+            break;
+        case 4:
+            cout << "Anda memilih fungsi dengan nilai kembali" << endl;
+            break;
+        case 5:
+            cout << "Anda memilih fungsi dengan nilai kembali dan argumen" << endl;
+            break;
+        case 6:
+            cout << "Anda memilih fungsi rekursif" << endl;
+            break;
+        case 7:
+            cout << "Anda memilih prototype" << endl;
+            break;
+        case 8:
+            cout << "Anda memilih operasi string" << endl;
+            break;
+        case 9:
+            cout << "Anda memilih array 1 dimensi" << endl;
+            break;
+        case 10:
+            cout << "Anda memilih array 2 dimensi" << endl;
+            break;
+        case 11:
+            cout << "Anda memilih array 3 dimensi" << endl;
+            break;
+        case 0:
+            cout << "Anda memilih keluar" << endl;
+            isRunning = false;
+            return 0;
+        default:
+            cout << "Pilihan anda tidak ada" << endl;
+            break;
+        }
+
+        return 0;
+    }
+}
+
+int prototype(int a, int b)
+{
+    cout << "Ini adalah prototype" << endl;
+    cout << "Nilai yang anda masukkan adalah " << a << " dan " << b << endl;
+    return a + b;
+}
