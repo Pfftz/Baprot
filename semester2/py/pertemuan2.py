1ee860a328a6da88f3e9c06299004056d2b2fa19
Pfftz <Kalixpfft+github@protonmail.com>
2024-02-28 13:43:37 +0700

    Delete unnecessary files and code

---
+++ semester2/py/pertemuan2.py
@@ -0,0 +1,11 @@
+angka = float(input("Masukkan angka: "))
+
+akar_kuadrat = angka ** 0.5
+
+print(f"Akar kuadrat dari {angka} adalah {akar_kuadrat}")
+
+alas = float(input("Masukkan alas: "))
+tinggi = float(input("Masukkan tinggi: "))
+luas_segitiga = 0.5 * alas * tinggi
+
+print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas_segitiga}")
