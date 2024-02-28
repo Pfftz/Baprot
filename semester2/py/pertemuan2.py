74669279fc428a19bbed144fa0be712a8fbc8a21
Pfftz <Kalixpfft+github@protonmail.com>
2024-02-28 13:55:37 +0700

    Refactor code and add grade calculation

--- semester2/py/pertemuan2.py
+++ semester2/py/pertemuan2.py
@@ -1,11 +1,38 @@
-angka = float(input("Masukkan angka: "))
+# angka = float(input("Masukkan angka: "))
 
-akar_kuadrat = angka ** 0.5
+# akar_kuadrat = angka ** 0.5
 
-print(f"Akar kuadrat dari {angka} adalah {akar_kuadrat}")
+# print(f"Akar kuadrat dari {angka} adalah {akar_kuadrat}")
 
-alas = float(input("Masukkan alas: "))
-tinggi = float(input("Masukkan tinggi: "))
-luas_segitiga = 0.5 * alas * tinggi
+# alas = float(input("Masukkan alas: "))
+# tinggi = float(input("Masukkan tinggi: "))
+# luas_segitiga = 0.5 * alas * tinggi
 
-print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas_segitiga}")
+# print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas_segitiga}")
+
+#input tugas, uts, uas
+tugas = float(input("Masukkan nilai tugas: "))
+uts = float(input("Masukkan nilai UTS: "))
+uas = float(input("Masukkan nilai UAS: "))
+
+#menghitung nilai akhir
+nilai_akhir = (0.15 * tugas) + (0.35 * uts) + (0.5 * uas)
+
+if nilai_akhir > 80:
+    grade = "A"
+elif nilai_akhir > 70:
+    grade = "B"
+elif nilai_akhir > 60:
+    grade = "C"
+elif nilai_akhir > 50:
+    grade = "D"
+else:
+    grade = "E"
+    
+#menentukan status kelulusan
+if nilai_akhir > 60:
+    status = "Lulus"
+else:
+    status = "Tidak Lulus"
+    
+print(f"Nilai akhir anda adalah {nilai_akhir} dengan grade {grade} dan status {status}")
