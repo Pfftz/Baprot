c24e31f05947a8e4583d795ca550b60e8c955105
Pfftz <Kalixpfft+github@protonmail.com>
2024-03-06 10:53:53 +0700

    Add Abdulhadi's personal information and create file reading functionality

---
+++ semester2/py/pertemuan3.py
@@ -0,0 +1,48 @@
+# '''KTM'''
+# print("===input Data====")
+# Nama = input("Nama: ")
+# Nim = input("Nim: ")
+# Fakultas = input("Fakultas: ")
+# Prodi = input("Prodi: ")
+# print("=================")
+# print("Nama: ", Nama)
+# print("Nim: ", Nim)
+# print("Fakultas: ", Fakultas)
+# print("Prodi: ", Prodi)
+# print("===Data Berhasil ditambahkan===")
+
+# '''cak'''
+# angka1 = input("Masukkan angka pertama: ")
+# angka2 = input("Masukkan angka kedua: ")
+
+# angka1 = int(angka1)
+# angka2 = int(angka2)
+
+# hasil_penjumlahan = angka1 + angka2
+# hasil_pengurangan = angka1 - angka2
+# hasil_perkalian = angka1 * angka2
+# hasil_pembagian = angka1 / angka2
+
+# print("Hasil penjumlahan: ", hasil_penjumlahan)
+# print("Hasil pengurangan: ", hasil_pengurangan)
+# print("Hasil perkalian: ", hasil_perkalian)
+# print("Hasil pembagian: ", hasil_pembagian)
+
+
+file = open("Abdulhadi_Muntashir_041_B.txt", "w")
+file.write("Nama = Abdulhadi\n")
+file.write("Nim = 3337230041 B\n")
+file.write("Prodi = IF\n")
+file.write("Fakultas = Teknik\n")
+file.write("Semester = 2\n")
+file.write("Tahun = 2024\n")
+file.write("Tempat Lahir = Cilegon\n")
+file.write("Kelamin = Laki-laki\n")
+file.write("Kampus = Untirta\n")
+file.write("Hobi = Main Game\n")
+file.write("Makanan Favorit = Nasi Goreng\n")
+file.write("Minuman Favorit = Es Teh\n")
+file.write("Motto = Jangan Menyerah\n")
+file.write("Cita-cita = Programmer\n")
+file.write("Alamat = Cilegon\n")
+file.close()
---
+++ semester2/py/versi_read.py
@@ -0,0 +1,11 @@
+# Open the file in read mode
+file = open("Abdulhadi_Muntashir_041_B.txt", "r")
+
+# Read the contents of the file
+contents = file.read()
+
+# Print the contents to the terminal
+print(contents)
+
+# Close the file
+file.close()
